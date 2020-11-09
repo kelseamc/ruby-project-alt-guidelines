66c7f61298a1a0151708751f4f3a27c07bc0f1d8
kelseamc <kelsea.mcall@gmail.com>
2020-11-09 16:58:32 -0500

    setting possible methods for user with tty-prompt

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,15 @@ class User < ActiveRecord::Base
     has_many :movies through :movie_watched
     # @@prompt = TTY::Prompt.new
 
-  
+    # def self.signup
+    #     username = @@prompt.ask("What is your name?")
+    #     password = @@prompt.mask("What is your password?")
+    #     self.create(username: username, password: password)
+    # end
+
+    # def self.login
+    #     username = @@prompt.ask("What is your name?")
+    #     password = @@prompt.mask("What is your password?")
+    #     self.find_by(username: username, password: password)
+    # end
 end
