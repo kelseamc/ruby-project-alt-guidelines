0cc41a1d019ae36c5962862109e9ab0edb866751
JasonAkiraJordan <jasonajordan0@gmail.com>
2020-11-09 17:18:25 -0500

    migrate

--- db/migrate/20201109215906_create_movies.rb
+++ db/migrate/20201109215906_create_movies.rb
@@ -2,7 +2,9 @@ class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
       t.string :title 
+      t.integer :genre_id
 
       t.timestamps
+    end 
   end
 end
