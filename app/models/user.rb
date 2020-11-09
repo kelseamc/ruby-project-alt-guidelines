5623c9fa41e0c2839f51d44b89503729f3d3e8f4
kelseamc <kelsea.mcall@gmail.com>
2020-11-09 16:35:26 -0500

    added possible sign up method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
     has many: genre_preferences
     has_many :genres through :genre_preferences
     has_many :movies through :movie_watched
+
+    # def self.signup
+    #     username = @@prompt.ask("What is your name?")
+    #     password = @@prompt.mask("What is your password?")
+    #     self.create(username: username, password: password)
+    # end
+
 end
