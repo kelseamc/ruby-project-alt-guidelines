18c50bc3b3cfedb6091106fc4848f070249305d2
kelseamc <kelsea.mcall@gmail.com>
2020-11-09 18:10:21 -0500

    seeds for genre, user, and movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,30 @@ Genre_preference.destroy_all
 # api_data.each { |cat| Category.create( api_id: cat["id"], title: cat["title"] )}
 # Category.create(api_data) NOT GONNA WORK B/C ADNTL KEY FROM API 
 
+g1 = Genre.create(name: "Action")
+g2 = Genre.create(name: "Comedy")
+g3 = Genre.create(name: "Drama")
+g4 = Genre.create(name: "Si-Fi")
+g5 = Genre.create(name: "Horror")
+g6 = Genre.create(name: "Thriller")
+g6 = Genre.create(name: "Romance")
+
+u1 = User.create(name: "Mike", password: "1234")
+u2 = User.create(name: "Tom", password: "1234")
+u3 = User.create(name: "Jess", password: "1234")
+
+m1 = Movie.create(title:"Taken" genre_id: g1.id)
+m2 = Movie.create(title:"The Hangover" genre_id: g2.id)
+m3 = Movie.create(title:"Just Mercy" genre_id: g3.id)
+m4 = Movie.create(title:"Star Trek" genre_id: g4.id)
+m5 = Movie.create(title:"Insidious" genre_id: g5.id)
+m6 = Movie.create(title:"The Notebook" genre_id: g6.id)
+
+
+
+
+
+
+
 binding.pry
 
