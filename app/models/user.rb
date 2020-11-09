499488c035304070c4fbe2a006eb4060b5686fd9
kelseamc <kelsea.mcall@gmail.com>
2020-11-09 16:43:41 -0500

    added possible use and require to tty-prompt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
+# require "tty-prompt"
 class User < ActiveRecord::Base
     has many: movie_watched
     has many: genre_preferences
     has_many :genres through :genre_preferences
     has_many :movies through :movie_watched
+    # @@prompt = TTY::Prompt.new
 
     # def self.signup
     #     username = @@prompt.ask("What is your name?")
