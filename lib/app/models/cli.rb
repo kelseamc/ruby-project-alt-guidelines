632a9ed2316d6162b47a12b28a09f6461acbad6c
JasonAkiraJordan <jasonajordan0@gmail.com>
2020-11-10 07:52:47 -0500

    prompt

--- lib/app/models/cli.rb
+++ lib/app/models/cli.rb
@@ -18,9 +18,9 @@ class CLI
         self.class.main_menu
     end
 
-    def run 
-        self.class.main_menu
-    end 
+    # def run 
+    #     self.class.main_menu
+    # end 
 
     def self.tty_prompt
         TTY::Prompt.new
@@ -30,6 +30,50 @@ class CLI
         system('clear')
         puts "testing"
         sleep(1)
+        puts "Welcome to (our not yet finshed app)"
+        #self.logo 
+        splash = self.tty_prompt.select("Please Log In or Sign Up!") do |prompt| 
+            prompt.choice "Log In"
+            prompt.choice "Sign Up"
+        end
+        case splash 
+        when "Log In"
+            self.login 
+        when "Sign Up"
+            self.signup 
+        end
+    end
+
+    def self.login 
+        prompt = self.tty_prompt 
+        username = prompt.ask("Username:")
+        password = prompt.mask("Password:")
+        self.testend
+        #@user = User.find_by(username: username, password: password)
+        # if @user 
+        #     system('clear')
+        #     self.login_main_menu 
+        # else
+        #     puts "Invalid username or password."
+        #     sleep(2)
+        #     system('clear')
+        #     self.main_menu  
+        # end
+    
+    end
+
+    def self.signup 
+        prompt = self.tty_prompt
+        username = prompt.ask("Username:")
+        password = prompt.mask("Password:")
+        self.testend
+        # @user = User.create(username: username, password: password)
+        # system('clear')
+        # self.login_main_menu
+    end
+
+    def self.testend
+        puts "End of Test"
     end 
 
 end 
