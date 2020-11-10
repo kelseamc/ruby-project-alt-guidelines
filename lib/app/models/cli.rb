1f9677825dd5cc5bc86cea586f7297e276d1757a
kelseamc <kelsea.mcall@gmail.com>
2020-11-10 12:53:55 -0500

    built favorite genres method

--- lib/app/models/cli.rb
+++ lib/app/models/cli.rb
@@ -10,12 +10,13 @@ class CLI
     @@artii = Artii::Base.new :font => 'slant'
     @@user = nil
 
+    #Welcome & user login --------
     def welcome
         system('clear')
         puts @@artii.asciify("Testing")
         puts @@artii.asciify("One two three")
-        sleep(2)
-        self.class.main_menu
+        #sleep(1)
+        self.class.login_menu
     end
 
     # def run 
@@ -26,10 +27,10 @@ class CLI
         TTY::Prompt.new
     end 
 
-    def self.main_menu 
+    def self.login_menu 
         system('clear')
-        puts "testing"
-        sleep(1)
+        #puts "testing"
+        #sleep(1)
         puts "Welcome to (our not yet finshed app)" 
         splash = self.tty_prompt.select("Please Log In or Sign Up!") do |prompt| 
             prompt.choice "Log In"
@@ -49,13 +50,14 @@ class CLI
         password = prompt.mask("Password:")
         #self.testend
         @user = User.find_by(username: username, password: password)
+        system('clear')
         if @user 
-            #system('clear')
-            self.testend
+            system('clear')
+            self.main_menu
             #self.next_menu 
         else
             puts "Invalid username or password."
-            sleep(2)
+            sleep(1)
             #system('clear')
             self.main_menu  
         end
@@ -68,14 +70,76 @@ class CLI
         password = prompt.mask("Password:")
         @user = User.create(username: username, password: password)
         #system('clear')
-        self.testend
+        self.main_menu
         # self.next_menu
     end
 
+
+    #Main Menu --------
+
+    def self.main_menu 
+        prompt = self.tty_prompt
+        splash = self.tty_prompt.select("What do want to do?") do |prompt| 
+        prompt.choice "Suggest a Movie"
+        prompt.choice "Genre Settings"
+        prompt.choice "My History"
+        prompt.choice "End"
+        end 
+        case splash 
+        when "Suggest a Movie"
+            self.moviesuggestor
+            #self.testend
+        when "Genre Settings"
+            #self.genre_menu
+            self.favorite_genres
+        when "My History"
+            #self.history
+            self.testend 
+        when "End"
+            self.testend 
+        end 
+
+    end 
+
+    def self.moviesuggestor #need to compare suggested movies to already watched
+        puts "test1"
+        @usermovies =[]
+        @user_pref = GenrePreference.where(user_id: @user.id)
+        system('clear')
+        @user_pref.each do |preference|
+            Movie.where(genre_id: preference.genre_id).each do |movie|
+                system('clear')
+                @usermovies << movie.title
+            end 
+        end 
+        p @usermovies.sample
+    end 
+
+    def self.favorite_genres
+        @user_genres = []
+        @user_pref = GenrePreference.where(user_id: @user.id)
+        system('clear')
+        @user_pref.map do |preference|
+            Genre.where(id: preference.genre_id).each do |genre|
+                system('clear')
+                @user_genres << genre.name
+            end
+        end
+        @user_genres.each {|genre| puts genre}
+    end
+
+    def self.genre_select_test # not done yet
+        prompt = self.tty_prompt 
+        genre_list = %w(horror comedy action)
+        result = prompt.multi_select("Select Genre", genre_list)
+        result
+    end 
+
     def self.testend
         puts "\n End of Test"
-        p User.all
+        #p User.all
         puts "End of Test"
     end 
 
+ #binding.pry
 end 
--- lib/app/models/genre_preference.rb
+++ lib/app/models/genre_preference.rb
@@ -1,4 +1,4 @@
 class GenrePreference < ActiveRecord::Base
-    # belongs_to :user
-    # belongs_to :genre
+    belongs_to :user
+    belongs_to :genre
 end
